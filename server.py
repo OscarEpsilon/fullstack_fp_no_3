f3e93de41f69ed3d9b58e4136e618444a6bf0e4f
OscarEpsilon <110500526+OscarEpsilon@users.noreply.github.com>
2025-01-30 05:07:00 -0500

    buggy disaster

--- server.py
+++ server.py
@@ -12,11 +12,11 @@ def home():
     return render_template("home.html")
 
 db = sqlite3.connect('database.db')
-db.execute('CREATE TABLE IF NOT EXISTS CHARACTERS (name TEXT, hp TEXT)')
+db.execute('CREATE TABLE IF NOT EXISTS CHARACTERS (name TEXT, level TEXT, hp TEXT, hp_tot TEXT, rm TEXT)')
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-    if request.method == 'POST':
+    if request.method == 'POST' and request.form['user']:
         user = request.form['user']
         return redirect(url_for('characters', name = user))
     else:
@@ -25,20 +25,23 @@ def login():
 @app.route("/characters/<name>", methods=['POST', 'GET'])
 def characters(name):
     if request.method == 'POST':
+        level = request.form['level'].replace(' ', '')
         hp = request.form['hp'].replace(' ', '')
+        hp_tot = request.form['hp_tot'].replace(' ', '')
+        rm = request.form['rm'].replace(' ', '')
         with sqlite3.connect("database.db") as users:
             cursor = users.cursor()
             if cursor.execute("SELECT * FROM CHARACTERS WHERE name=\'" + name + "\'").fetchall():
-                cursor.execute("UPDATE CHARACTERS SET hp = \'" + hp + "\' WHERE name=\'" + name + "\'")
+                cursor.execute("UPDATE CHARACTERS SET level = \'" + level + "\' hp = \'" + hp + "\' hp_tot = \'" + hp_tot + "\' rm = \'" + rm + "\' WHERE name=\'" + name + "\'")
             else:
-                cursor.execute("INSERT INTO CHARACTERS (name,hp) VALUES (?,?)", (name, hp))
+                cursor.execute("INSERT INTO CHARACTERS (name,level,hp,hp_tot,rm) VALUES (?,?,?,?,?)", (name, level, hp, hp_tot, rm))
             users.commit()
     connect = sqlite3.connect('database.db')
     cursor = connect.cursor()
     cursor.execute("SELECT * FROM CHARACTERS WHERE name=\'" + name + "\'")
     data = cursor.fetchall()
     if not data:
-        data = [('0','0')]
+        data = [('0','0','0','0','0')]
     print(data)
     return render_template("sheet.html",name = name, character = data)
 
